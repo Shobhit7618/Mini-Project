5815e81eebe473fb53ea83aa04b82afd25feeaec
Shobhit7618 <shobhityadav9919@gmail.com>
2023-10-04 12:09:11 +0530

    Mini Project

---
+++ backend/routers/connection.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const url = "mongodb+srv://shobhit:123@cluster0.ytww1xx.mongodb.net/miniproject?retryWrites=true&w=majority";
+
+//asynchronous function - return Promise object
+
+mongoose.connect(url)
+.then((result) => {
+    console.log('Database Connected');    
+})
+.catch((err) => {
+    console.log(err);
+});
+
+// console.log('Another Statement');
+
+module.exports=mongoose;
---
+++ backend/routers/index.js
@@ -0,0 +1,47 @@
+//import express
+const express=require('express');
+const userRouter=require('./routers/userRouter');
+const productRouter=require('./routers/productRouter');
+const cors = require('cors');
+//initialise express
+const app=express();
+const port=5000;
+
+//middlewares
+
+app.use(express.json());
+app.use(cors({
+    origin:['http://localhost:3000']
+}));
+
+app.use('/user',userRouter);
+app.use('/product',productRouter);
+
+//routes
+app.get('/',(req,res)=>{
+    res.send('response from express')
+});
+
+app.get('/home',(req,res)=>{
+    res.send('response from home')
+});
+app.get('/add',(req,res)=>{
+    res.send('response from Add')
+});
+app.get('/getall',(req,res)=>{
+    res.send('response from Getall')
+});
+app.get('/getbyid',(req,res)=>{
+    res.send('response from GetbyId')
+});
+app.get('/update',(req,res)=>{
+    res.send('response from Update')
+});
+app.get('/delete',(req,res)=>{
+    res.send('response from Delete')
+});
+
+//add,getall
+app.listen(port,()=>{
+    console.log('server started');
+});
